40d41d6f97d9b15c26f204b83bca6b917d8e4df8
Yura Zenevich <yura.zenevich@gmail.com>
2018-09-07 18:37:36 -0400

    Create addon for configuring e10s preferences for OLDJAWS users. (#126)

---
+++ addons/jaws-esr/bootstrap.js
@@ -0,0 +1,86 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* exported install, uninstall, startup, shutdown */
+/* eslint no-implicit-globals: "off" */
+
+"use strict";
+
+ChromeUtils.import("resource://gre/modules/Services.jsm");
+ChromeUtils.import("resource://gre/modules/AppConstants.jsm");
+
+const A11Y_INIT_OR_SHUTDOWN = "a11y-init-or-shutdown";
+
+const PREF_BROWSER_TABS_REMOTE_FORCE_DISABLE = "browser.tabs.remote.force-disable";
+
+const CONFIRM_RESTART_PROMPT_RESTART_NOW = 0;
+
+const observer = {
+  observe(subject, topic, data) {
+    if (topic === "a11y-init-or-shutdown" && data === "1") {
+      checkVersionPromptAndDisableE10S(); // eslint-disable-line no-use-before-define
+    }
+  }
+};
+
+function removeA11yInitOrShutdownObserver() {
+  try {
+    Services.obs.removeObserver(observer, A11Y_INIT_OR_SHUTDOWN);
+  } catch (e) {
+    // Accessibility init or shutdown event observer might not have been initialized if:
+    // * A11y service was never used
+    // * A11y service was enabled at startup
+    // * Observer was already removed on a11y service init.
+  }
+}
+
+async function checkVersionPromptAndDisableE10S() {
+  // User is assumed to not use OLDJAWS or e10s is already disabled.
+  if (!Services.appinfo.shouldBlockIncompatJaws ||
+      !Services.appinfo.browserTabsRemoteAutostart) {
+    return;
+  }
+
+  removeA11yInitOrShutdownObserver();
+
+  const title = "Restart Firefox";
+  const msg = "Accessibility support is partially disabled due to compatibility " +
+              "issues with new Firefox features. Please restart Firefox to disable " +
+              "features conflicting with accessibility support.";
+  const restartButtonText = "Restart Firefox now";
+
+  let buttonFlags = (Services.prompt.BUTTON_POS_0 *
+                     Services.prompt.BUTTON_TITLE_IS_STRING);
+  buttonFlags += (Services.prompt.BUTTON_POS_1 * Services.prompt.BUTTON_TITLE_CANCEL);
+  buttonFlags += Services.prompt.BUTTON_POS_0_DEFAULT;
+
+  const buttonIndex = Services.prompt.confirmEx(null, title, msg, buttonFlags,
+    restartButtonText, null, null, null, {});
+
+  if (buttonIndex === CONFIRM_RESTART_PROMPT_RESTART_NOW) {
+    Services.prefs.setBoolPref(PREF_BROWSER_TABS_REMOTE_FORCE_DISABLE, true);
+    Services.startup.quit(Ci.nsIAppStartup.eAttemptQuit | Ci.nsIAppStartup.eRestart);
+  }
+}
+
+function install() {}
+
+function uninstall() {}
+
+function startup() {
+  // Do nothing if we are not on Windows or if e10s is disabled.
+  if (AppConstants.platform !== "win" || !Services.appinfo.browserTabsRemoteAutostart) {
+    return;
+  }
+
+  if (Services.appinfo.accessibilityEnabled) {
+    checkVersionPromptAndDisableE10S();
+  } else {
+    Services.obs.addObserver(observer, A11Y_INIT_OR_SHUTDOWN);
+  }
+}
+
+function shutdown() {
+  removeA11yInitOrShutdownObserver();
+}
